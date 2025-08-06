f69a0ad200238632f9cfc0bd13d750a6ea45ee4d
YoshiBleu9427 <nicolas118218@msn.com>
2025-08-06 23:27:09 +0200

    Rebalance contract gen: remove longest streaks, uber streaks; generally increase points

--- src/contracts/common/contract_gen.py
+++ src/contracts/common/contract_gen.py
@@ -6,70 +6,67 @@ from contracts.common.models import Contract, GameClass, User
 # TODO test assert all enum options are here
 
 COMMON_CONTRACTS: dict[ContractType, list[tuple[int, int]]] = {
-    ContractType.KILLS: [(5, 12), (15, 40), (25, 75)],
-    ContractType.ROUNDS_PLAYED: [(2, 5), (3, 10), (5, 16)],
-    ContractType.ROUNDS_WON: [(1, 5), (2, 11), (3, 18)],
+    ContractType.KILLS: [(5, 12), (12, 40), (20, 75)],
+    ContractType.ROUNDS_PLAYED: [(1, 5), (2, 10), (3, 16)],
+    ContractType.ROUNDS_WON: [(1, 10), (2, 20), (3, 35)],
 }
 RARE_CONTRACTS: dict[ContractType, list[tuple[int, int]]] = {
-    ContractType.CAPTURES: [(3, 15), (7, 30), (12, 60)],
-    ContractType.DOMINATIONS: [(2, 20), (3, 35), (5, 60)],
-    ContractType.KILL_STREAK: [(3, 20), (4, 30), (5, 50)],
+    ContractType.CAPTURES: [(3, 20), (5, 40), (8, 75)],
+    ContractType.DOMINATIONS: [(1, 20), (2, 35), (4, 60)],
+    ContractType.KILL_STREAK: [(2, 20), (3, 30), (4, 50)],
 }
 CLASS_CONTRACTS: dict[GameClass, dict[ContractType, list[tuple[int, int]]]] = {
     GameClass.RUNNER: {
-        ContractType.KILLS_AS_CLASS: [(3, 9), (5, 16), (10, 35)],
-        ContractType.KILLS_ON_CLASS: [(2, 12), (3, 20), (4, 30)],
-        ContractType.CAPTURES: [(3, 15), (5, 20), (7, 30), (12, 60)],
+        ContractType.KILLS_AS_CLASS: [(3, 15), (5, 30)],
+        ContractType.KILLS_ON_CLASS: [(2, 20), (3, 32), (4, 45)],
+        ContractType.CAPTURES: [(3, 20), (5, 40), (8, 75), (12, 120)],
     },
     GameClass.FIREBUG: {
-        ContractType.KILLS_AS_CLASS: [(3, 9), (5, 16), (10, 35)],
-        ContractType.KILLS_ON_CLASS: [(2, 12), (3, 20), (4, 30)],
-        ContractType.BURN_DURATION: [(4, 3), (6, 6), (10, 12), (15, 20)],
-        ContractType.FLARE_KILLS: [(1, 8), (2, 18), (3, 30)],
+        ContractType.KILLS_AS_CLASS: [(3, 15), (5, 30)],
+        ContractType.KILLS_ON_CLASS: [(2, 20), (3, 32), (4, 45)],
+        ContractType.BURN_DURATION: [(4, 6), (6, 12), (10, 24), (15, 40)],
+        ContractType.FLARE_KILLS: [(1, 20), (2, 50), (3, 80)],
     },
     GameClass.ROCKETMAN: {
-        ContractType.KILLS_AS_CLASS: [(3, 9), (5, 16), (10, 35)],
-        ContractType.KILLS_ON_CLASS: [(2, 12), (3, 20), (4, 30)],
-        ContractType.DAMAGE_TAKEN: [(3, 4), (5, 10), (7, 20), (9, 50)],
+        ContractType.KILLS_AS_CLASS: [(3, 15), (5, 30)],
+        ContractType.KILLS_ON_CLASS: [(2, 20), (3, 32), (4, 45)],
+        ContractType.DAMAGE_TAKEN: [(3, 5), (5, 15), (6, 30), (7, 80)],
     },
     GameClass.OVERWEIGHT: {
-        ContractType.KILLS_AS_CLASS: [(3, 9), (5, 16), (10, 35)],
-        ContractType.KILLS_ON_CLASS: [(2, 12), (3, 20), (4, 30)],
-        ContractType.DAMAGE_TAKEN: [(3, 4), (5, 10), (7, 20), (9, 50)],
+        ContractType.KILLS_AS_CLASS: [(3, 15), (5, 30)],
+        ContractType.KILLS_ON_CLASS: [(2, 20), (3, 32), (4, 45)],
+        ContractType.DAMAGE_TAKEN: [(3, 5), (5, 15), (6, 30), (7, 80)],
     },
     GameClass.DETONATOR: {
-        ContractType.KILLS_AS_CLASS: [(3, 9), (5, 16), (10, 35)],
+        ContractType.KILLS_AS_CLASS: [(3, 15), (5, 30)],
         ContractType.FLYING_STICKY: [(2, 12), (3, 20), (4, 30)],
     },
     GameClass.HEALER: {
-        ContractType.HEALING: [(12, 6), (18, 12), (25, 18), (100, 70)],
-        ContractType.UBERS: [(2, 6), (5, 15)],
+        ContractType.HEALING: [(12, 10), (18, 20), (30, 30)],
+        ContractType.UBERS: [(2, 9), (5, 25)],
         ContractType.HEAL_STREAK: [(5, 6), (8, 18), (10, 30), (12, 60)],
-        ContractType.UBERED_KILLS: [(2, 20), (3, 35), (4, 50), (5, 75)],
-        ContractType.UBERED_STREAK: [(2, 35)],
+        ContractType.UBERED_KILLS: [(2, 20), (3, 35), (4, 50)],
     },
     GameClass.CONSTRUCTOR: {
-        ContractType.AUTOGUN_KILLS: [(3, 10), (6, 22), (10, 40)],
+        ContractType.AUTOGUN_KILLS: [(3, 15), (5, 30), (7, 45)],
         ContractType.AUTOGUN_STREAK: [(2, 15)],
-        ContractType.GUN_KILLS: [(2, 8), (5, 25), (10, 60)],
+        ContractType.GUN_KILLS: [(2, 15), (3, 25), (5, 45)],
     },
     GameClass.RIFLEMAN: {
-        ContractType.NOSCOPE_KILLS: [(3, 12), (5, 22), (10, 45)],
-        ContractType.KILLS_AS_CLASS: [(3, 9), (5, 16), (10, 35)],
-        ContractType.DOMINATIONS: [(2, 20), (3, 35), (5, 60)],
-        ContractType.KILL_STREAK: [(3, 20), (4, 30), (5, 50)],
+        ContractType.NOSCOPE_KILLS: [(3, 18), (5, 35), (7, 50)],
+        ContractType.KILLS_AS_CLASS: [(3, 15), (5, 30)],
+        ContractType.DOMINATIONS: [(1, 20), (2, 35), (4, 60)],
+        ContractType.KILL_STREAK: [(2, 20), (3, 30), (4, 50)],
     },
     GameClass.INFILTRATOR: {
-        ContractType.KILLS_AS_CLASS: [(3, 9), (5, 16), (10, 35)],
+        ContractType.KILLS_AS_CLASS: [(3, 15), (5, 30)],
         ContractType.STABS: [(2, 14), (3, 20), (5, 36)],
         ContractType.GUN_KILLS: [(2, 10), (5, 25), (10, 60)],
-        ContractType.CAPTURES: [(3, 15), (10, 50)],
-        ContractType.KILL_STREAK: [(2, 15), (3, 20), (4, 30)],
+        ContractType.KILL_STREAK: [(2, 20), (3, 30), (4, 50)],
     },
     GameClass.QUOTE: {
-        ContractType.KILLS_AS_CLASS: [(2, 9), (3, 15), (5, 30)],
-        ContractType.BUBBLE_SHIELD: [(3, 6), (5, 10), (7, 16), (10, 25)],
-        ContractType.CAPTURES: [(3, 15), (10, 50)],
+        ContractType.KILLS_AS_CLASS: [(3, 15), (5, 30)],
+        ContractType.BUBBLE_SHIELD: [(3, 10), (5, 20), (7, 30)],
     },
 }
 
